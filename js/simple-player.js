c8fd4f5d42b32d38d75989d4bd7e4b9fe8468415
simpleton rabs <74567810+simpleton-rabs@users.noreply.github.com>
2024-03-03 01:06:48 +0000

    final

--- js/simple-player.js
+++ js/simple-player.js
@@ -3,109 +3,90 @@ const button = document.getElementById("play-pause-button");
 const currentTime = document.getElementById("current-time");
 const totalTime = document.getElementById("total-time");
 const seekBar = document.getElementById("seek-bar");
+
 // Variable added to link to volume bar
 const volumeBar = document.getElementById("volume-bar");
+
+//does not matter if volume bar is enabled before track loads
 volumeBar.max = 1;
 volumeBar.min = 0;
+//smaller steps more accurate
 volumeBar.step = 0.01;
+//full volume default on load
 volumeBar.value = 1;
-// const songOne = document.getElementById("track-one");
-// const songTwo = document.getElementById("track-two");
-// const songThree = document.getElementById("track-three");
 
-//collection with different tracks
+//add collection with different tracks
 const trackOption = document.getElementsByClassName("track-options");
 
-//index of selected track
+//index of selected track and set selected colour
 let selected = 0;
 const selectedColour = "gray";
-// const audio = new Audio("audio/Soft-Background-for-Interview.webm");
 
-// set default track on load
+// set default track on load and set selected option on track menu
 const audio = new Audio(trackOption[selected].dataset.track);
 trackOption[selected].style.backgroundColor = selectedColour;
 
+//boolean set to false - not seeking
 let seeking = false;
+
 //event handlers
+
 //button events
+//if button clicked when playing or paused
 button.onclick = () => {
-    if(audio.paused){
+    if (audio.paused) {
         audio.play();
-    }else{
+    } else {
         audio.pause();
     }
 }
 
-// songOne.onclick = () => {
-//     if(audio.play){
-//         audio.pause();
-//         audio.src = songOne.dataset.song;
-//     }
-// }
-// songTwo.onclick = () => {
-//     if(audio.play){
-//         audio.pause();
-//         audio.src = songTwo.dataset.song;
-//     }
-// }
-// songThree.onclick = () => {
-//     if(audio.play){
-//         audio.pause();
-//         audio.src = songThree.dataset.song;
-//     }
-// }
-
-for (i = 0; i < trackOption.length; i++){
+//for loop for event listners if track option selected
+for (i = 0; i < trackOption.length; i++) {
     trackOption[i].onclick = trackClick
 }
 
-
-
 // audio events
-audio.onloadedmetadata = function(){
+//when track loads
+audio.onloadedmetadata = function () {
     currentTime.innerHTML = formatTime(0);
     totalTime.innerHTML = formatTime(audio.duration);
     seekBar.max = Math.floor(audio.duration);
     seekBar.value = 0;
-
-    // volumeBar.disabled = false;
-
-
-    // volumeBar.max = 1;
-    // volumeBar.min = 0;
-    // volumeBar.step = 0.1;
-    // volumeBar.value = 1;
 }
 
+//when track loads we can play and seek bar can be active
 audio.oncanplaythrough = () => {
     seekBar.disabled = false;
-    
 }
 
+//when audio plays buttons shows pause
 audio.onplay = () => {
     button.src = "images/pause.svg"
 }
 
+// when audion pauses button shows play
 audio.onpause = () => {
     button.src = "images/play.svg"
 }
 
+//time as track plays
 audio.ontimeupdate = () => {
     currentTime.innerHTML = formatTime(audio.currentTime);
-    if(!seeking){
+    if (!seeking) {
         seekBar.value = Math.floor(audio.currentTime)
     }
 }
 
 //seek bar events
 
-seekBar.oninput = () =>{
+seekBar.oninput = () => {
     seeking = true;
 }
 seekBar.onchange = () => {
     audio.currentTime = seekBar.value;
-    if(!audio.paused){
-    audio.play();
+    if (!audio.paused) {
+        audio.play();
     }
     seeking = false;
 }
@@ -116,52 +97,47 @@ volumeBar.onchange = () => {
     audio.volume = volumeBar.value;
 }
 
-function trackClick(event){
-//     // set selection on track menu bar change button colour
+//function for if track option clicked
+function trackClick(event) {
+    // variable to be set to integer of track selection via function returnIndex 
     let newSelected = returnIndex(this);
-    if (newSelected != selected){
+    // if new selected track isn't current selected track
+    if (newSelected != selected) {
+        // set selected track number to new one
         selected = newSelected
+        //call function to set selected track colour
         setSelectionColour(this)
     }
+    //update src to be new track selection and play - defaults from time 00:00
     audio.src = this.dataset.track;
     audio.play();
+}
 
-    // if (audio.play){
-    //     audio.play()
-    // } else{
-    //     audio.pause()
-    // }
-
-    }
-
-//change track src to selected - stops play and seek defaults to 0:00 but button needs to be reset
-    // audio.src = this.dataset.track;
-    // button.src = "images/play.svg";
-// }
-
+//function to return integer to index current track and check correct
 function returnIndex(trackOptions) {
-    for(i=0; i < trackOption.length; i++){
+    for (i = 0; i < trackOption.length; i++) {
         if (trackOption[i] == trackOptions) {
             // console.log(i)
-        return i   
+            return i
         }
     }
-        console.warn("ITEM NOT FOUND")
-        return -1
-    }
-function setSelectionColour (trackOptions){
-    for(i=0; i < trackOption.length; i++)
-    if (trackOption[i] == trackOptions){
-    trackOption[i].style.backgroundColor = selectedColour        
-    }else{
+    console.warn("ITEM NOT FOUND")
+    return -1
+}
+
+//function to set selected track option colour
+function setSelectionColour(trackOptions) {
+    for (i = 0; i < trackOption.length; i++)
+        // if selected track matches track-options than this is selection to be updated
+        if (trackOption[i] == trackOptions) {
+            // set selection colour
+            trackOption[i].style.backgroundColor = selectedColour
+        } else {
+            //else remove selection colour
             trackOption[i].style.backgroundColor = ""
         }
 }
 
-// trackOption[i].style.backgroundColor = selectedColour
-// else{
-//     trackOption[i].style.backgroundColor = "none"
-// }
 // takes total seconds (number) and returns a formatted string 
 function formatTime(secs) {
     let hours = Math.floor(secs / 3600);
